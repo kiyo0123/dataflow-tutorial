9abd9a348ccb11df0c62c2b8235fb3028ba8855d
kiyo0123 <kiyoshi.fukuda@gmail.com>
2018-02-20 17:01:41 +0900

    source code added

---
+++ generate_project.sh
@@ -0,0 +1,11 @@
+mvn archetype:generate \
+      -DarchetypeArtifactId=google-cloud-dataflow-java-archetypes-examples \
+      -DarchetypeGroupId=com.google.cloud.dataflow \
+      -DarchetypeVersion=2.2.0 \
+      -DgroupId=com.example \
+      -DartifactId=first-dataflow \
+      -Dversion="0.1" \
+      -DinteractiveMode=false \
+      -Dpackage=com.example
+
+
